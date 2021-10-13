62210378fe6613b304ec1ce561dfde88fec555b4
Joschua Becker <jb@scolasti.co>
2021-10-13 10:26:50 +0200

    added debug

--- src/main/java/me/scolastico/runner/manager/etc/CommandExecuter.java
+++ src/main/java/me/scolastico/runner/manager/etc/CommandExecuter.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import me.scolastico.runner.manager.Application;
 
 public class CommandExecuter {
 
   public static String[] run(String command) throws InterruptedException, IOException {
+    if (Application.getConfig().isDebug()) System.out.println("[debug] Executing command: " + command);
     Runtime run = Runtime.getRuntime();
     Process pr = run.exec(command);
     pr.waitFor();
